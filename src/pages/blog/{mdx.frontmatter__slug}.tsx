2072364dc8271bf88d0c9d7f9cbe9c149408a61a
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-08-27 17:31:02 +0900

    #10.15 GatsbyImage

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import Seo from "../../components/Seo";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 interface IBlogPostProps {
   data: Queries.PostDetailQuery;
   children: any;
 }
 
-export default function BlogPost({ children }: IBlogPostProps) {
+export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -24,8 +29,13 @@ export const query = graphql`
         author
         category
         date
-        slug
         title
+        slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
